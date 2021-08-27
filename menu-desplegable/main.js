1c70a3588b8ce7d339bdab2d79b642151e7a98fd
Javier Armando Vargas Vega <javieravargasestudiante@gmail.com>
2021-08-27 18:13:43 -0500

    Commit Final

---
+++ menu-desplegable/main.js
@@ -0,0 +1,23 @@
+let listElements = document.querySelectorAll('.list_button--click');
+console.log(listElements);
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', () => {
+        // alert();
+        // le agregamos y quitamos la clase arrow, cada vez que demos click
+        listElement.classList.toggle('arrow'); 
+
+        let height = 0;
+        // tomamos el hermano adyacente
+        let menu = listElement.nextElementSibling;
+        console.log(menu.scrollHeight);
+
+        if (menu.clientHeight == "0") {
+            height = menu.scrollHeight;
+        }
+
+        // menu.style.height = height + "px";
+        menu.style.height = `${height}px`;
+    })
+})
+
