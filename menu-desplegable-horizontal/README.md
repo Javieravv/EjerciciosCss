8dd5f68fbf80f5e6efb8abfd21f3d47327917961
Javier Armando Vargas Vega <javieravargasestudiante@gmail.com>
2021-08-30 20:39:50 -0500

    Commit menu desplegable Horizontal

---
+++ menu-desplegable-horizontal/README.md
@@ -0,0 +1,15 @@
+#Menú desplegable horizontal
+
+Páginas web usadas:
+
+1. www.boxicons.com
+2. https://fonts.google.com/ con la fuente poppins.
+3. https://bennettfeely.com/clippy/
+
+4. Ejercicio con base en el video de YouTube de esta dirección:
+
+https://www.youtube.com/watch?v=6437QeuvbpY&list=WL&index=1&t=60s
+
+
+
+
---
+++ menu-desplegable-horizontal/js/app.js
@@ -0,0 +1,63 @@
+/* Esta función se ejecuta a sí msWriteProfilerMark
+
+ (function ()) {
+   
+
+ }) ();
+*/
+
+
+(function () {
+    const listElements = document.querySelectorAll('.menu__item--show');
+    const list = document.querySelector('.menu__links');
+    const menu = document.querySelector('.menu__hamburguer');
+
+    console.log(menu);
+
+    const addClick = () => {
+        listElements.forEach (element => {
+            element.addEventListener('click', () => {
+                let subMenu = element.children[1];
+                let height = 0;
+                element.classList.toggle('menu__item--active');
+                // console.log (subMenu.clientHeight); 
+                if (subMenu.clientHeight === 0) {
+                    height = subMenu.scrollHeight;
+                }
+                subMenu.style.height = `${height}px`;
+            })
+        })
+    }
+
+    const deleteStyleHeight = () => {
+        listElements.forEach(element =>{
+            if (element.children[1].getAttribute('style')) {
+                element.children[1].removeAttribute('style');
+                element.classList.remove('menu__item--active');
+            }
+        })
+    }
+
+    // addClick();
+
+    /*Creamos una función para que cuando se cambie el tamaño de la pantalla
+    no se rompa el aplicativo*/ 
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 800) {
+            deleteStyleHeight();
+            if (list.classList.contains('menu__links--show'))
+                list.classList.remove('menu__links--show');
+        } else {
+            addClick();
+        }
+    })
+
+    if (window.innerWidth <= 800) {
+        addClick();
+    }
+
+    menu.addEventListener('click', () => list.classList.toggle('menu__links--show'));
+    
+}) ();
+
